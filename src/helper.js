88d3793912aafe8217fd7db151fcf6b7d9ee8073
Martin Gabel <martin.gabel@fiebig.com>
2017-04-18 16:54:17 +0200

    karibik

--- src/helper.js
+++ src/helper.js
@@ -1,5 +1,178 @@
-function foo(bar) {
-    print(bar);
+function Hex (q, r) {
+  this.q = +q
+  this.r = +r
+}
+Hex.prototype.toString = function () {
+  return `H(${this.q}, ${this.r})`
 }
 
-module.exports = foo;
+function Cube (x, y, z) {
+  this.x = +x
+  this.y = +y
+  this.z = +z
+}
+Cube.prototype.toString = function () {
+  return `C(${this.x}, ${this.y}, ${this.z})`
+}
+
+function Offset (col, row) {
+  this.col = +col
+  this.row = +row
+}
+Offset.prototype.toString = function () {
+  return `O(${this.col}, ${this.row})`
+}
+
+Cube.prototype.toOffset = function () {
+  return new Offset(
+    this.x + (this.z - (this.z & 1)) / 2,
+    this.z
+  )
+}
+
+Cube.prototype.toHex = function () {
+  return new Hex(this.x, this.z)
+}
+
+Hex.prototype.toCube = function () {
+  const x = this.q
+  const z = this.r
+  const y = -x - z
+  return new Cube(x, y, z)
+}
+
+Offset.prototype.toCube = function () {
+  const x = this.col - (this.row - (this.row & 1)) / 2
+  const z = this.row
+  const y = -x - z
+  return new Cube(x, y, z)
+}
+
+Cube.prototype.add = function (cube) {
+  if (cube instanceof Cube) {
+    return new Cube(
+      this.x + cube.x,
+      this.y + cube.y,
+      this.z + cube.z
+    )
+  }
+  throw new Error(`Cube: cannot add, cube expected`)
+}
+
+Hex.prototype.add = function (hex) {
+  if (hex instanceof Hex) {
+    return new Hex(
+      this.q + hex.q,
+      this.r + hex.r
+    )
+  }
+  throw new Error(`Hex: cannot add, hex expected`)
+}
+
+Cube.Directions = [
+  new Cube(1, -1, 0), new Cube(1, 0, -1), new Cube(0, 1, -1),
+  new Cube(-1, 1, 0), new Cube(-1, 0, 1), new Cube(0, -1, 1)
+]
+
+Cube.prototype.neighbour = function (direction) {
+  return this.add(Cube.Directions[direction])
+}
+
+Hex.Directions = [
+  new Hex(1, 0), new Hex(1, -1), new Hex(0, -1),
+  new Hex(-1, 0), new Hex(-1, 1), new Hex(0, 1)
+]
+
+Hex.prototype.neighbour = function (direction) {
+  return this.add(Hex.Directions[direction])
+}
+
+Cube.prototype.distance = function (cube) {
+  if (cube instanceof Cube) {
+    return Math.max(
+      Math.abs(this.x - cube.x),
+      Math.abs(this.y - cube.y),
+      Math.abs(this.z - cube.z)
+    )
+  }
+  throw new Error('Cube: cannot calc distance, Cube expected')
+}
+
+Cube.prototype.round = function () {
+  let rx = Math.round(this.x)
+  let ry = Math.round(this.y)
+  let rz = Math.round(this.z)
+  let xd = Math.abs(rx - this.x)
+  let yd = Math.abs(ry - this.y)
+  let zd = Math.abs(rz - this.z)
+
+  if (xd > yd && xd > zd) {
+    rx = -ry - rz
+  } else if (yd > zd) {
+    ry = -rx - rz
+  } else {
+    rz = -rx - ry
+  }
+  return new Cube(rx, ry, rz)
+}
+
+const lerp = (a, b, t) => {
+  return a + (b - a) * t
+}
+
+Cube.prototype.lerp = function (b, t) {
+  const a = this
+  return new Cube(
+    lerp(+a.x, +b.x, +t),
+    lerp(+a.y, +b.y, +t),
+    lerp(+a.z, +b.z, +t)
+  ).round()
+}
+
+Cube.prototype.line = function (b) {
+  const N = this.distance(b)
+  const line = []
+  for (let i = 0; i <= N; ++i) {
+    line.push(this.lerp(b, 1.0 / N * i))
+  }
+  return line
+}
+
+Cube.prototype.isEqual = function (b) {
+  const {x, y, z} = this
+  return x == b.x && y == b.y && z == b.z
+}
+
+Cube.prototype.directionTo = function (b) {
+  const line = this.line(b)
+  if (line.length <= 1) return -1
+  const next = line[1]
+  for (let i = 0; i < Cube.Directions.length; ++i) {
+    const n = this.neighbour(i)
+    if (n.isEqual(next)) return i
+  }
+  return -1
+}
+
+Cube.prototype.inRange = function (N) {
+  const results = []
+  for (let dx = -N; dx <= N; ++dx) {
+    for (let dy = Math.max(-N, -dx - N); dy <= Math.min(N, -dx + N); ++dy) {
+      const dz = -dx - dy
+      results.push(this.add(new Cube(dx, dy, dz)))
+    }
+  }
+  return results
+}
+
+const rand = (min, max) => {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min)) + min
+}
+module.exports = {
+  Hex,
+  Cube,
+  Offset,
+  rand
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,143 @@
-/* global readline, print*/
-const foo = require('./helper');
-
-const n = parseInt(readline());
-for (let i = 0; i < n; ++i) {
-	const inputs = readline().split(' ');
-	const x = Number(inputs[0]);
-	const y = Number(inputs[1]);
+/* global readline print printErr */
+const h = require('./helper')
+const DEBUG = false
+const log = (...args) => {
+  if (!DEBUG) return
+  printErr(...args)
 }
 
-print('hallo');
+const MAP_W = 23
+const MAP_H = 21
+const SHIP = 'SHIP'
+const BARREL = 'BARREL'
+const CANNONBALL = 'CANNONBALL'
+const MINE = 'MINE'
+const ROTATION = 0
+const SPEED = 1
+const STOCK = 2
+const OWNER = 3
+const RUM = 0
+const ME = 1
+const SHOOTER = 1
+const TTL = 2
+
+function Entity (data) {
+  const [id, type, x, y, arg1, arg2, arg3, arg4] = data.split(' ')
+  this.id = +id
+  this.type = type
+  this.data = [+arg1, +arg2, +arg3, +arg4]
+  this.pos = new h.Offset(x, y).toCube()
+}
+
+Entity.prototype.toString = function () {
+  return Object.keys(this).map(key => `${key}: ${this[key]}`).join(', ')
+}
+
+function generateEntities (count) {
+  const entities = []
+  for (let i = 0; i < count; ++i) {
+    entities.push(new Entity(readline()))
+  }
+  return entities
+}
+
+const isShip = x => x.type === SHIP
+const isBarrel = x => x.type === BARREL
+const isMine = x => x.data[OWNER] === ME
+const isAMine = x => x.type === MINE
+const isCannonball = x => x.type === CANNONBALL
+const byDistance = start => (a, b) => {
+  return (start.distance(a.pos) < start.distance(b.pos))
+    ? -1
+    : (start.distance(a.pos) > start.distance(b.pos))
+    ? 1
+    : 0
+}
+
+const isOnPosition = pos => x => x.pos.isEqual(pos)
+
+const targetPrediction = (ship, enemy) => {
+  const cannonPos = ship.pos.neighbour(ship.data[ROTATION])
+  let ttt = Math.round(1 + cannonPos.distance(enemy.pos) / 3)
+  let next = enemy.pos
+  for (let i = 0; i < ttt; ++i) {
+    for (let j = 0; j < enemy.data[SPEED]; ++j) {
+      next = next.neighbour(enemy.data[ROTATION])
+    }
+  }
+  return next
+}
+
+while (true) {
+  const myShipCount = Number(readline())
+  const entityCount = Number(readline())
+  const entities = generateEntities(entityCount)
+  const myShips = entities.filter(x => isShip(x) && isMine(x))
+  const barrels = entities.filter(isBarrel)
+  const cannonballs = entities.filter(isCannonball)
+  const mines = entities.filter(isAMine)
+  const enemyShips = entities.filter(x => isShip(x) && !isMine(x))
+  myShips.forEach(ship => {
+    log(ship)
+    const rotation = ship.data[ROTATION]
+
+    const barrel = barrels.sort(byDistance(ship.pos)).shift()
+    const myCannonballs = cannonballs.filter(x => x.data[SHOOTER] === ship.id)
+    const nearestEnemy = enemyShips.sort(byDistance(ship.pos))[0]
+    const distanceToEnemy = ship.pos.neighbour(ship.data[ROTATION]).distance(nearestEnemy.pos)
+    let action = 'MOVE'
+    let message = 'dododo'
+    let target = barrel
+      ? barrel.pos
+      : ship.pos.neighbour(rotation)
+
+    if (ship.data[SPEED] === 0) {
+      action = 'MOVE'
+    }
+    if (ship.data[STOCK] < 50) {
+      action = 'MOVE'
+    } else {
+      if (distanceToEnemy <= 10 && myCannonballs.length <= 0) {
+        target = targetPrediction(ship, nearestEnemy)
+        action = 'FIRE'
+      } else {
+        const direction = (rotation + 1) % 6
+        target = ship.pos.neighbour(rotation).neighbour(direction)
+        message = 'idle'
+      }
+    }
+
+
+    log('mines')
+    const danger = mines.concat(cannonballs).filter(m => {
+      log('mine', m)
+      const distance = ship.pos.distance(m.pos)
+      const direction = ship.pos.directionTo(m.pos)
+      return distance <= 3 && (direction === -1 || direction === rotation)
+    }).concat(enemyShips.filter(s => ship.pos.distance(s.pos) <= 3))
+
+    if (danger.length > 0) {
+      const distance = ship.pos.distance(danger[0].pos)
+      let direction = ship.pos.directionTo(danger[0].pos)
+      if (distance <= 2 && direction !== rotation) {
+        target = ship.pos.neighbour(rotation).neighbour(rotation)
+        message = 'close danger not in path'
+      } else if (direction === rotation) {
+        direction = (rotation + 1) % 6
+        target = ship.pos.neighbour(direction).neighbour(direction)
+        message = 'close danger in path'
+      } else {
+        direction = (direction + 3) % 6
+        target = danger[0].pos.neighbour(direction).neighbour(direction).neighbour(direction)
+        message = 'some danger'
+      }
+      action = 'MOVE'
+    }
+    target = target.toOffset()
+    if (target.col < 0) target.col = MAP_W - 1
+    if (target.col >= MAP_W) target.col = 0
+    if (target.row < 0) target.row = MAP_H - 1
+    if (target.row >= MAP_H) target.row = 0
+    print(`${action} ${target.col} ${target.row} ${message}`)
+  })
+}
